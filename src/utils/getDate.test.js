cb5cfa977d98a118424464428d95f419420fe847
BiswajitSen <sen229126@gmail.com>
2024-01-27 18:26:09 +0530

    Add test case against areDateSame, asserts over different dates

--- src/utils/getDate.test.js
+++ src/utils/getDate.test.js
@@ -10,6 +10,15 @@ describe("Date", () => {
     ).toEqual(true);
   });
 
+  it("should result in false for different dates", () => {
+    expect(
+      areDatesSame(
+        { day: 10, month: 11, year: 10 },
+        { day: 10, month: 10, year: 10 }
+      )
+    ).toEqual(false);
+  });
+
   it("should parse day month and year from a given date", () => {
     const dateInfo = getDateInformation("2024-01-27T11:52:19.862Z");
     expect(dateInfo).toStrictEqual({ year: 2024, month: 1, day: 27 });
